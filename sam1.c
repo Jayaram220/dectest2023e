578edcf8067ea0debc23a1f88716f499c290fef6
Jayaram220 <jayaram220@gmail.com>
2023-12-10 18:33:42 +0530

    added

--- sam1.c
+++ sam1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+void sum(int a,int b)
+{
+	int sum = a+b;
+	printf("sum = %d",sum);
+
+}
+
+void fun3()
+{
+	printf("rrrrrrrrrrrrrr");
+}
+int main()
+{
+	printf("jppppppppppppp");
+}
